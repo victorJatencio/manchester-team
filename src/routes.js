320cf90ad4203d419b63830297d53a5f8b3f837e
Victor Atencio <victoratencio@Victors-MBP.fios-router.home>
2020-02-20 06:34:59 -0500

    almost done creating header component

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "./Hoc/Layout";
+
+const Routes = props => {
+  return (
+    <div>
+      <Layout>second line</Layout>
+    </div>
+  );
+};
+
+export default Routes;
