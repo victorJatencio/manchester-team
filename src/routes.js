b08a48adbbaaa7c5ca94551f985ea0b4b485c8de
victor atencio <victorj.atencio@gmail.com>
2020-04-02 20:23:23 -0400

    Created Admin matches table

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "./Hoc/Layout";
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 
 import PrivateRoutes from "./components/authRoutes/privateRoutes";
 import PublicRoutes from "./components/authRoutes/publicRoutes";
@@ -9,12 +9,19 @@ import Home from "./components/home";
 import SignIn from "./components/signin";
 
 import Dashboard from "./components/admin/Dashboard";
+import AdminMatches from "./components/admin/matches";
 
 const Routes = props => {
   return (
     <div>
       <Layout>
         <Switch>
+          <PrivateRoutes
+            {...props}
+            exact
+            component={AdminMatches}
+            path="/admin_matches"
+          />
           <PrivateRoutes
             {...props}
             exact
